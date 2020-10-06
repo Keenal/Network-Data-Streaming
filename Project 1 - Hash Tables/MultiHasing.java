7425e9333be9759d4e7ffbafa9d679e4e9411573
Keenal <keenal.shah@ufl.edu>
2020-10-06 03:18:29 -0400

    add multi hashing

---
+++ Project 1 - Hash Tables/MultiHasing.java
@@ -0,0 +1,58 @@
+public class MultiHasing {
+    public static final int numOfEntries = 1000; 
+    public static final int numOfHashFunction = 3; 
+
+    public static int[] hashTable = new int[numOfEntries];
+    public static int[] hashModNums = {737, 27522, 84569};
+
+    public static void main(String[] args) {
+
+        initHashTable(hashTable);
+
+        addInTable(hashTable);
+
+        printHashTable(hashTable);
+
+    }
+
+    public static void initHashTable(int[] hashTable) {
+        for(int i = 0; i < hashTable.length; i++) {
+            hashTable[i] = -999999999; 
+        }
+    }
+
+    public static void addInTable(int[] hashTable) {
+        for(int i = 0; i < numOfEntries; i++) {
+            int element = (int) (Math.random()*1000);
+            int[] hashValues = new int[numOfHashFunction];
+
+            for(int j = 0; j < hashValues.length; j++) {
+                hashValues[j] = (element ^ hashModNums[j]) % hashTable.length; 
+                if(hashTable[hashValues[j]] == element) {
+                    break;
+                }
+            }
+
+            for(int j = 0; j < hashValues.length; j++) {
+                
+                if(hashTable[hashValues[j]] == -999999999) {
+                    hashTable[hashValues[j]] = element;
+                    break;
+                }
+            }
+        }
+
+    }
+
+    public static void printHashTable(int[] hashTable) {
+        int count = 0;
+        for(int i = 0; i < hashTable.length; i++) {
+            if(hashTable[i] != -999999999) {
+                count++;
+            }
+        }
+        System.out.println("count " + count);
+    }
+
+
+}
